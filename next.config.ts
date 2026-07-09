fd1c3af106f092c20cfa9c5336cddab462176bb2
Abdallah <abdallahshehhabb@gmail.com>
2026-07-09 18:24:29 +0300

    Fix next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -11,14 +11,14 @@ const nextConfig: NextConfig = {
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
-    },
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  
+
+
+
+
   typescript: {
     ignoreBuildErrors: true,
-  },
+
 };
 
 export default withSerwist(nextConfig);
